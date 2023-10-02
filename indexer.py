7652276345ea2668a40d198a06f6e571e82b10bc
Adam Prochazka <adam@Adam-MacBook-Air.local>
2023-10-02 13:37:47 +0100

    Add persistence

--- indexer.py
+++ indexer.py
@@ -1,15 +1,12 @@
 from reader import Reader
-import os
-
+import os, persistence
 
 
 class Indexer:
+    persistence = persistence.JsonDictionaryPersistence()
 
-    _book_names = {
-    }
-
-    _word_indexes = {
-    }
+    _book_names = {}
+    _word_indexes = {}
 
     def _index_one(self, path, index):
         reader = Reader()
@@ -21,7 +18,6 @@ class Indexer:
                 self._word_indexes[word] = {index}
             else:
                 self._word_indexes[word].add(index)
-
 
     def index_all(self, directory):
         """
@@ -35,7 +31,6 @@ class Indexer:
     def get_indexes_of_word(self, word):
         return self._word_indexes[word]
 
-
     def get_book_name(self, index):
         return self._book_names[index]
 
@@ -45,9 +40,14 @@ class Indexer:
             book_names.append(self._book_names[index])
         return book_names
 
+    def dump(self, book_names_path="book_names.json", word_index_path="word_indexes.json"):
+        self.persistence.dump_dict(book_names_path, self._book_names)
+        serialized_dict = {key: list(value) for key, value in self._word_indexes.items()}
+        self.persistence.dump_dict(word_index_path, serialized_dict)
 
+    def load(self, book_names_path="book_names.json", word_index_path="word_indexes.json"):
+        loaded_book_names = self.persistence.load_dict(book_names_path)
+        self._book_names = {int(key): value for key, value in loaded_book_names.items()}
 
-indexer = Indexer()
-indexer.index_all("datalake")
-print(indexer._book_names)
-print(indexer.get_book_names_for_indexes(indexer.get_indexes_of_word("Yellow")))
+        loaded_word_indexes = self.persistence.load_dict(word_index_path)
+        self._word_indexes = {key: set(value) for key, value in loaded_word_indexes.items()}
---
+++ persistence.py
@@ -0,0 +1,11 @@
+import json
+
+class JsonDictionaryPersistence:
+
+    def dump_dict(self, path, dictionary):
+        with open(path, 'w') as file:
+            json.dump(dictionary, file)
+
+    def load_dict(self, path):
+        with open(path, 'r') as file:
+            return json.load(file)
