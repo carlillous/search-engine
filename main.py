69f9eac884e187d7b5582bb974b969d3d451cfa9
Adam Prochazka <adam.prochazka@icloud.com>
2023-10-05 14:26:47 +0100

    Delete tests from main.py

--- main.py
+++ main.py
@@ -1,21 +1,2 @@
 # Branch Adam
 # Branch Carlos the first
-from databaseIndexer import DatabaseIndexer
-
-# Usage
-directory_path = "datalake"  # Provide the desired directory path
-
-indexer = DatabaseIndexer()
-# indexer.index_all(directory_path)
-# indexer.index_one("datalake/B1.txt", 0)
-# indexer.index_one("datalake/B2.txt", 0)
-print(indexer.get_list_of_books_for_word("hello"))
-print(indexer.get_list_of_books_for_word("yes"))
-print(indexer.get_list_of_books_for_word("hi"))
-print(indexer.get_books())
-# print(indexer.get_words())
-# print(indexer.get_books_words())
-
-# Close the database connection when done
-
-indexer.close()
