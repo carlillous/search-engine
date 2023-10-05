6e069b62389a62c92910438ccbe85c6863952fbf
Adam Prochazka <adam.prochazka@icloud.com>
2023-10-05 14:10:49 +0100

    Add test implementation of databaseIndexer

--- databaseIndexer.py
+++ databaseIndexer.py
@@ -1,53 +1,104 @@
+import os
+import sqlite3
 from reader import Reader
-import os, persistence
+
+class DatabaseIndexer:
+    def __init__(self, db_file = "indexer_sqlite.db"):
+        self.conn = sqlite3.connect(db_file)
+        self.cursor = self.conn.cursor()
+        self._create_tables()
+
+    def _create_tables(self):
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS books (
+                book_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                book_name TEXT
+            )
+        ''')
+
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS words (
+                word_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                word_text TEXT UNIQUE
+            )
+        ''')
+
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS books_words (
+                book_id INTEGER,
+                word_id INTEGER,
+                UNIQUE(book_id, word_id),
+                FOREIGN KEY (book_id) REFERENCES books(book_id),
+                FOREIGN KEY (word_id) REFERENCES words(word_id)
+            )
+        ''')
+
+        self.conn.commit()
 
 
-class DictionaryIndexer:
-    persistence = persistence.JsonDictionaryPersistence()
-
-    _book_names = {}
-    _word_indexes = {}
-
-    def _index_one(self, path, index):
+    def index_one(self, path, book_index):
         reader = Reader()
         book_name, words = reader.read_book(path)
-        self._book_names[index] = book_name
+        print(f"[INFO]: Indexing book \"{book_name}\"")
+        self.cursor.execute('INSERT INTO books (book_name) VALUES (?)', (book_name,))
+
+        book_id = self.cursor.lastrowid
+        # print(f"[INSERTED]: Book = \"{book_name}\" with id: {book_id}")
 
         for word in words:
-            if word not in self._word_indexes:
-                self._word_indexes[word] = {index}
-            else:
-                self._word_indexes[word].add(index)
+            # print(f"[INFO]: inserting word = \"{word}\" into book = \"{book_name}\"")
+            self.cursor.execute('INSERT OR IGNORE INTO words (word_text) VALUES (?)', (word,))
+
+            self.cursor.execute("SELECT word_id FROM words WHERE word_text = ?", (word,))
+            word_id = self.cursor.fetchone()[0]
+            print(f"[INSERTED]: word = \"{word}\", id = {word_id}, book = \"{book_name}\"")
+
+            self.cursor.execute('INSERT OR IGNORE INTO books_words VALUES (?, ?)', (book_id, word_id))
+        self.conn.commit()
 
     def index_all(self, directory):
-        """
-        """
         for i, filename in enumerate(os.listdir(directory)):
             file = os.path.join(directory, filename)
-            # checking if it is a file
             if os.path.isfile(file):
-                self._index_one(file, i)
+                self.index_one(file, i)
 
-    def get_indexes_of_word(self, word):
-        return self._word_indexes[word]
+    def get_list_of_books_for_word(self, word):
+        self.cursor.execute('''
+            SELECT book_name
+            FROM books_words
+            JOIN books ON books.book_id = books_words.book_id
+            JOIN words ON words.word_id = books_words.word_id
+            WHERE word_text = ?
+        ''', (word,))
 
-    def get_book_name(self, index):
-        return self._book_names[index]
+        return self.cursor.fetchall()
+        # return [name[0] for name in self.cursor.fetchall()]
 
-    def get_book_names_for_indexes(self, indexes):
-        book_names = []
-        for index in indexes:
-            book_names.append(self._book_names[index])
-        return book_names
 
-    def dump(self, book_names_path="book_names.json", word_index_path="word_indexes.json"):
-        self.persistence.dump_dict(book_names_path, self._book_names)
-        serialized_dict = {key: list(value) for key, value in self._word_indexes.items()}
-        self.persistence.dump_dict(word_index_path, serialized_dict)
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
 
-    def load(self, book_names_path="book_names.json", word_index_path="word_indexes.json"):
-        loaded_book_names = self.persistence.load_dict(book_names_path)
-        self._book_names = {int(key): value for key, value in loaded_book_names.items()}
+    def get_books(self):
+        self.cursor.execute('''
+               SELECT *
+               FROM books
+           ''')
 
-        loaded_word_indexes = self.persistence.load_dict(word_index_path)
-        self._word_indexes = {key: set(value) for key, value in loaded_word_indexes.items()}
+        return self.cursor.fetchall()
+
+    def get_words(self):
+        self.cursor.execute('''
+               SELECT *
+               FROM words
+           ''')
+
+        return self.cursor.fetchall()
+
+    def get_books_words(self):
+        self.cursor.execute('''
+            SELECT *
+            FROM books_words
+           ''')
+
+        return self.cursor.fetchall()
