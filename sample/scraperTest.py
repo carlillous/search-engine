abbca89ab10f344e8bef6870acef049ecdb1f2d7
Carlillous <carlosmenegg@gmail.com>
2023-10-06 15:21:59 +0100

    Added scraperTest and also metadata on/off in webScraper.

---
+++ sample/scraperTest.py
@@ -0,0 +1,9 @@
+from webScraper import WebScraper
+
+"""
+Sample usage of the web Scraper.
+"""
+
+scraper1 = WebScraper(number=5, d_name="datalake1")
+
+scraper2 = WebScraper(number=5, d_name="datalake2", md=False)
