e11b1c707ef39769f7626404c58502d7d37d93f2
adam-prochazka <adam.prochazka@icloud.com>
2023-10-09 13:38:01 +0100

    Add DatabaseIndexer sample

---
+++ sample/db_indexer_sample.py
@@ -0,0 +1,18 @@
+from src.database_indexer import DatabaseIndexer
+from src.dictionary_indexer import DictionaryIndexer
+
+BOOKS_PATH_MAC = "../datalake/book_content"
+BOOKS_PATH_WIN = "???"
+
+BOOKS_PATH = BOOKS_PATH_MAC
+QUERY_WORD = "island"
+
+"""
+Sample usage of database indexer.
+- note: Run close() after using the indexer to stop connection with db.
+"""
+
+db_indexer = DatabaseIndexer()
+db_indexer.index_all(BOOKS_PATH)
+print(db_indexer.get_list_of_books_for_word(QUERY_WORD))
+db_indexer.close()
