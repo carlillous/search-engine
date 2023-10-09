c897443465100a8b772154b93fda7ce4e2c1b786
adam-prochazka <adam.prochazka@icloud.com>
2023-10-09 13:37:39 +0100

    Remove db_indexer from dictionary indexer sample

--- sample/dict_indexer_sample.py
+++ sample/dict_indexer_sample.py
@@ -8,16 +8,6 @@ BOOKS_PATH = BOOKS_PATH_MAC
 QUERY_WORD = "island"
 
 """
-Sample usage of database indexer.
-- note: Run close() after using the indexer to stop connection with db.
-"""
-
-db_indexer = DatabaseIndexer()
-db_indexer.index_all(BOOKS_PATH)
-print(db_indexer.get_list_of_books_for_word(QUERY_WORD))
-db_indexer.close()
-
-"""
 Sample usage of dictionary indexer.
 """
 
