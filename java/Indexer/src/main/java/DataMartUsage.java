14c4a8034706c115fc38300042923779472a6c30
adam-prochazka <adam.prochazka@icloud.com>
2023-10-26 18:00:18 +0100

    Replace FileDataMart with DataMart in Indexer

--- java/Indexer/src/main/java/DataMartUsage.java
+++ java/Indexer/src/main/java/DataMartUsage.java
@@ -1,6 +1,6 @@
 public class DataMartUsage {
     public static void main(String[] args) {
-        FileDataMart dataMart = new FileDataMart();
+        DataMart dataMart = new FileDataMart();
 
         String word = "hello";
         int bookIndex = 0;
