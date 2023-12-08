3b43670530323c8f8c8fe80f2062d88747e4f44c
JoelClemente <joel.lopez106@alu.ulpgc.es>
2023-12-08 17:29:46 +0000

    Hazelcast

---
+++ java/Indexer/src/main/java/HazelcastMain.java
@@ -0,0 +1,8 @@
+import datalake.DataLake;
+
+public class HazelcastMain {
+    public static void main(String[] args) {
+        HazelcastIndexer indexer = new HazelcastIndexer(new DataLake());
+        indexer.indexQueue();
+    }
+}
