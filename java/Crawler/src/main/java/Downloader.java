c2367be5da8995d4cc7bc363b967e9e081bcc42c
Carlillous <carlosmenegg@gmail.com>
2023-12-08 19:29:04 +0000

    Crawler fixed + logging info.

--- java/Crawler/src/main/java/Downloader.java
+++ java/Crawler/src/main/java/Downloader.java
@@ -3,9 +3,12 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import java.io.IOException;
 import java.util.Random;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Downloader {
     private DataLake dataLake;
+    private static final Logger logger = LogManager.getLogger(Downloader.class);
 
     public Downloader(DataLake dl){
         dataLake = dl;
@@ -17,14 +20,15 @@ public class Downloader {
                 int randomNumber = random.nextInt(70000) + 1;
                 String numStr = String.valueOf(randomNumber);
                 String bookUrl = "https://www.gutenberg.org/cache/epub/" + numStr + "/pg" + numStr + ".txt";
+                logger.info("Hello world");
 
                 if (dataLake.isBookInDataLake(numStr)) {
-                    System.out.println("Book is already downloaded.");
+                    logger.info("Book is already downloaded.");
                 }else{
                     if (downloadBook(bookUrl, numStr)) {
-                        System.out.println("Download completed.");
+                        logger.info("Download completed.");
                     } else {
-                        System.out.println("Book not found: " + bookUrl);
+                        logger.info("Book not found: " + bookUrl);
                     }
                 }
         } catch (Exception e) {
@@ -34,7 +38,7 @@ public class Downloader {
 
     private boolean downloadBook(String bookUrl, String i) {
         try {
-            Connection.Response response = Jsoup.connect(bookUrl).ignoreContentType(true)
+            Connection.Response response = Jsoup.connect(bookUrl).timeout(15000).ignoreContentType(true)
                     .execute();
             String book = response.parse().wholeText();
 
@@ -48,7 +52,7 @@ public class Downloader {
 
                 dataLake.saveToFile(dataLake.getDataLakePath() + fileName, bookContent);
                 MessageSender.sendMessage(fileName);
-                System.out.println("Book saved: " + fileName);
+                logger.info("Book saved: " + fileName);
                 return true;
             } else {
                 return false;
