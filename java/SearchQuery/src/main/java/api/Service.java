9c6b5c667854587dbec2940583e899b9baaecddd
adam-prochazka <adam.prochazka@icloud.com>
2023-12-09 16:34:30 +0000

    Add log4j to search query

--- java/SearchQuery/src/main/java/api/Service.java
+++ java/SearchQuery/src/main/java/api/Service.java
@@ -6,17 +6,20 @@ import impl.file.FileDataMart;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Service {
     private final DataMart dataMart = new FileDataMart();
     private final DataLake dataLake = new DataLake();
     private final JSONSerializer serializer = new JSONSerializer();
+    private static final Logger logger = LoggerFactory.getLogger(Service.class);
 
     public String search(String word) {
         List<Integer> indexes = dataMart.getInvertedIndexOf(word);
         List<String> names = indexes.stream().map(dataLake::getTitle).collect(Collectors.toList());
 
-        System.out.println("[API-SERVICE]: REQ = \"" + word +"\" -> " + names);
+        logger.info("REQ = \"" + word +"\" -> " + names);
 
         SearchRequest req = new SearchRequest(word, names);
         return serializer.toJson(req);
