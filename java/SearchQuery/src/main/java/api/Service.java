8fd09711c1af11b36dd1b41d482afda0cabd7c66
adam-prochazka <adam.prochazka@icloud.com>
2023-12-14 22:14:25 +0000

    Refactor SearchQuery

--- java/SearchQuery/src/main/java/api/Service.java
+++ java/SearchQuery/src/main/java/api/Service.java
@@ -1,24 +1,26 @@
 package api;
 
 
-import datalake.filesystem.DataLake;
-import impl.DataMart;
-import impl.file.FileSystemDataMart;
+import hazelcast.DataMart;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Service {
-    private final DataMart dataMart = new FileSystemDataMart();
-    private final DataLake dataLake = new DataLake();
+    private final DataMart dataMart = new DataMart();
     private final JSONSerializer serializer = new JSONSerializer();
     private static final Logger logger = LoggerFactory.getLogger(Service.class);
 
     public String search(String word) {
         List<Integer> indexes = dataMart.getInvertedIndexOf(word);
-        List<String> names = indexes.stream().map(dataLake::getTitle).collect(Collectors.toList());
+        List<String> names = indexes.stream()
+            .map(dataMart::getBookName)
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .collect(Collectors.toList());
 
         logger.info("REQ: \"" + word +"\" -> " + names);
 
